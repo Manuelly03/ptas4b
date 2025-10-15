e895933d1aec557a18a6d952665dae31816809bb
Manuelly Fernandes <229393998+Manuelly03@users.noreply.github.com>
2025-10-15 16:32:18 -0400

    semana 5

--- semana 5/src/server.js
+++ semana 5/src/server.js
@@ -2,9 +2,19 @@ import http from 'node:http'
 
 //criar um usuario  (nome, email, senha)
 
-const server = http.createServer((req, res) =>(
-  return res.end("Hello Word")
-))
+
+const server = http.createServer((req, res) => {
+    const {method,url} = req
+
+    if(url == "/user" && method == "GET" ){
+        return res.end("Buscar usuarios")
+    }
+    if (url == "/user" && method == "POST"){
+        return res.end("Criar usuario")
+    }
+  return res.end("Hello Wolds")
+}
+)
 
 server.listen(3333)
 
